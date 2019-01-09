46c47f5de1280304dd9432eea62a582692d23ac7
Robbie Baker <robbiethebakerman@googlemail.com>
2019-01-09 19:24:49 +0000

    Adds getWhiskiesByYear

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
@@ -1,5 +1,31 @@
 package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.EntityManager;
+import java.util.List;
 
 public class WhiskyRepositoryImpl implements WhiskyRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+
+    public List<Whisky> getAllWhiskiesByYear(int year) {
+        List<Whisky> results = null;
+        Session session = entityManager.unwrap(Session.class);
+
+        try {
+            Criteria cr = session.createCriteria(Whisky.class);
+            cr.add(Restrictions.eq("year", year));
+            results = cr.list();
+        } catch(HibernateException ex) {
+            ex.printStackTrace();
+        }
+
+        return results;
+    }
 }
